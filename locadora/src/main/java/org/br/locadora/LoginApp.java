ef2a3b598d37dd997dcfe0e9e1eec48cedef0a9c
Guilherme <guilherme.freitas@pcinformatica.com.br>
2015-03-12 20:07:01 -0300

    Login funcionando

--- locadora/src/main/java/org/br/locadora/LoginApp.java
+++ locadora/src/main/java/org/br/locadora/LoginApp.java
@@ -2,8 +2,10 @@ package org.br.locadora;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class LoginApp extends Application {
@@ -13,15 +15,28 @@ public class LoginApp extends Application {
 	}
 
 	@Override
-	public void start(Stage primarystage) throws Exception {
-		Label label = new Label();
-		label.setText("Hello Word");
-		HBox box = new HBox();
-		box.getChildren().add(label);
-		primarystage.setScene(new Scene(label));
-		primarystage.setWidth(500);
-		primarystage.setHeight(400);
-		primarystage.show();
+	public void start(Stage stage) throws Exception {
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(400, 300);
+		pane.setStyle("-fx-background-color: linear-gradient( from 0% 0% to 100% 100%, blue 0%, white 100%);");
+		TextField txLogin = new TextField();
+		txLogin.setPromptText("Digite aqui seu login");
+		PasswordField txSenha = new PasswordField();
+		txSenha.setPromptText("Digite aqui sua senha");
+		Button btEntrar = new Button("Entrar");
+		Button btSair = new Button("Sair");
+		pane.getChildren().addAll(txLogin, txSenha, btEntrar, btSair);
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.show();
+		txLogin.setLayoutX((pane.getWidth() - txLogin.getWidth()) / 2);
+		txLogin.setLayoutY(50);
+		txSenha.setLayoutX((pane.getWidth() - txSenha.getWidth()) / 2);
+		txSenha.setLayoutY(100);
+		btEntrar.setLayoutX((pane.getWidth() - btEntrar.getWidth()) / 2);
+		btEntrar.setLayoutY(150);
+		btSair.setLayoutX((pane.getWidth() - btSair.getWidth()) / 2);
+		btSair.setLayoutY(200);
 	}
 
 	public static void main(String[] args) {
